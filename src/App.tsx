35023aed6bb7ae2f18e05afbd1bfbe2408b2687e
Songyi-Park <132335084+park-songyi@users.noreply.github.com>
2026-05-16 23:45:29 +0900

    [#3] CodeMirror, Acorn 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <>App</>;
-}
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 
+function App() {
+  const [value, setValue] = React.useState("console.log('hello world!');");
+
+  const onChange = React.useCallback((val: string) => {
+    console.log("val:", val);
+    setValue(val);
+  }, []);
+
+  return (
+    <CodeMirror
+      value={value}
+      width="400px"
+      height="200px"
+      extensions={[javascript({ jsx: true })]}
+      onChange={onChange}
+    />
+  );
+}
 export default App;
