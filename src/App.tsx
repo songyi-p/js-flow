820ddd19dba846b98412b920702140fe4cbf4bf9
Songyi-Park <97720036+songyi-p@users.noreply.github.com>
2026-05-18 18:29:14 +0900

    [#9] zustand, immer 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
-import "./styles/global.css";
 
 function App() {
   const [value, setValue] = React.useState("console.log('hello world!');");
